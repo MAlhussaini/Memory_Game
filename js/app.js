951fc76f644d4a8949c0da800123347470d4b190
MAlhussaini <MAlhussaini56@gmail.com>
2019-12-09 19:37:25 +0300

    All cards toggle

--- js/app.js
+++ js/app.js
@@ -1,29 +1,85 @@
 /* Core Features:
-1 - Start screen to take the name (Player as default name) and start button.
-2 - Shuffle all the cards. 
-3 - All Cards must be flipped down at the start of the game.
-4 - A click on a card flip it faced up.
-5 - If two cards clicked then latch, or hide.
-6 - Create a list that holds all of opened cards
-7 - Each 2 clicks is one move. 
-8 - Time start at the start of the game.
-9 - Stars are calculated based on a formula. 
-10- At the end of the game, popup shows the stats and name of player.
+*1 - Start screen to take the name (Player as default name) and start button.
+*2 - Shuffle all the cards. 
+*3 - All Cards must be flipped down at the start of the game.
+*4 - A click on a card flip it faced up.
+*5 - If two cards clicked then latch, or hide.
+*6 - Create a list that holds all of opened cards
+*7 - Each 2 clicks is one move. 
+*8 - Time start at the start of the game.
+*9 - Restarting the game is possible. 
+*10- Stars are calculated based on a formula. 
+*11- At the end of the game, popup shows the stats and name of player.
+
+Bonus:
+* Flipping card animation.
+* Game start: Show all the cared faced up for 3s.
+* Shake the cards OR make the background color red on wrong answers.
+* if the cards match, make animation and make the background green.
+* Flipping card animation.
+* Leaderboard
 */
 
-// TODO: If the cards match, both cards stay flipped over.
-// TODO: If the cards do not match, both cards are flipped face down.
-// TODO: Show all the cared faced up for 3s.
-// TODO: Shake the cards OR make the background color red on wrong answers.
-// TODO: Create a timer for answering.
-// TODO: Calculate the number of moves player makes.
-// TODO: if the cards match, make animation and make the background green.
-// TODO: Game ending screen/popup.
-// TODO: Flipping card animation.
-// TODO: Random shuffling of the cards at the start of the game.
-// TODO: Create a restart button.
-// TODO: Make a stare rating based on predefined formula. 
-// TODO: Leaderboard
-
-
 // The start of the code. 
+
+// Toggle the cards
+let cards = document.querySelectorAll(".card");
+
+function toggleShow () {
+    for (const element of cards) {
+        element.classList.toggle("show");
+        console.log("toggle executed");
+    }
+
+}
+
+setTimeout(toggleShow,500);
+setTimeout(toggleShow,3000);
+
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// Small scale code.
+// Selecting the first card
+
+function cardSelect(element) {
+    element.classList.toggle("show");
+    element.classList.toggle("open");        
+}
+function cardLatch(array) {
+    for (const element of array) {
+        element.classList.toggle("show");
+        element.classList.toggle("open");        
+        element.classList.toggle("match") ;
+    }
+    
+}
+
+let card = document.querySelector(".card");
+
+// setTimeout(toggleShow(cards),100000);
+// window.setTimeout(toggleShow(cards),20000);
+
+
+/* 
+let firstCard = document.querySelector(".card").classList;
+console.log(firstCard);
+firstCard.toggle("show");
+firstCard.toggle("open");
+firstCard = document.querySelector(".card").classList;
+console.log(firstCard); 
+*/
